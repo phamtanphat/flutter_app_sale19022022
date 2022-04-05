5d460728e0c8505b2a266a37bb0c06ba18cc0cee
phamtanphat <phatdroid94@gmail.com>
2022-04-05 21:10:44 +0700

    ui login screen

--- lib/common/app_constant.dart
+++ lib/common/app_constant.dart
@@ -1,4 +1,8 @@
 class AppConstant{
   // Name route
   static const String LOGIN_ROUTE_NAME = "/login";
+
+
+  //Image path
+  static const String BANNER_1 = "assets/images/banner_1.jpg";
 }
--- lib/presentation/features/login/login_screen.dart
+++ lib/presentation/features/login/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-class LoginScreen extends StatefulWidget {
+import 'package:flutter/widgets.dart';
+import 'package:flutter_app_sale19022022/common/app_constant.dart';
 
+class LoginScreen extends StatefulWidget {
   LoginScreen({Key? key}) : super(key: key);
 
   @override
@@ -10,6 +12,154 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return LoginContainer();
+  }
+}
+
+class LoginContainer extends StatefulWidget {
+  const LoginContainer({Key? key}) : super(key: key);
+
+  @override
+  State<LoginContainer> createState() => _LoginContainerState();
+}
+
+class _LoginContainerState extends State<LoginContainer> {
+  final _emailController = TextEditingController();
+
+  final _passController = TextEditingController();
+
+  var isPassVisible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Login System"),
+        ),
+        body: Container(
+          color: Colors.white,
+          child: SafeArea(
+            child: CustomScrollView(
+              slivers: [
+                SliverFillRemaining(
+                  hasScrollBody: true,
+                  child: Stack(
+                    fit: StackFit.loose,
+                    children: [
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Expanded(
+                              flex: 2,
+                              child: Image.asset(AppConstant.BANNER_1)),
+                          Expanded(
+                            flex: 4,
+                            child: Container(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  _buildPhoneTextField(),
+                                  _buildPasswordTextField(),
+                                  _buildButtonSignIn(),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(child: _buildTextSignUp()),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget _buildTextSignUp() {
+    return Container(
+        margin: EdgeInsets.only(left: 10, right: 10),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Don't have an account!"),
+            InkWell(
+              onTap: () {},
+              child: Text("Sign Up",
+                  style: TextStyle(
+                      color: Colors.red, decoration: TextDecoration.underline)),
+            )
+          ],
+        ));
+  }
+
+  Widget _buildPhoneTextField() {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: TextField(
+        maxLines: 1,
+        controller: _emailController,
+        keyboardType: TextInputType.emailAddress,
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          fillColor: Colors.black12,
+          filled: true,
+          hintText: "Email",
+          labelStyle: TextStyle(color: Colors.blue),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(width: 0, color: Colors.black12)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(width: 0, color: Colors.black12)),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(width: 0, color: Colors.black12)),
+          prefixIcon: Icon(Icons.email, color: Colors.blue),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: TextField(
+        maxLines: 1,
+        controller: _passController,
+        obscureText: isPassVisible,
+        keyboardType: TextInputType.text,
+        textInputAction: TextInputAction.done,
+        decoration: InputDecoration(
+          fillColor: Colors.black12,
+          filled: true,
+          hintText: "PassWord",
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(width: 0, color: Colors.black12)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(width: 0, color: Colors.black12)),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(width: 0, color: Colors.black12)),
+          labelStyle: TextStyle(color: Colors.blue),
+          prefixIcon: Icon(Icons.lock, color: Colors.blue),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButtonSignIn() {
+    return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text("Sign In"),
+        ));
   }
 }
